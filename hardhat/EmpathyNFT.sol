8118995512fe0cf4150a01b7ebbbfb0b6442a9e5
gksmfly <149565853+gksmfly@users.noreply.github.com>
2025-08-12 00:28:18 +0900

    Update EmpathyNFT.sol

--- hardhat/EmpathyNFT.sol
+++ hardhat/EmpathyNFT.sol
@@ -2,21 +2,18 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-/**
- * 목적: 검열/PII 마스킹/점수 통과(>=0.70)한 글을 tokenURI로 기록.
- * - owner(=배포자)만 민팅 가능: 서버가 PRIVATE_KEY로 서명해서 실행.
- * - tokenURI(string)에는 data:application/json;base64, ... 메타데이터를 넣음.
- */
-contract EmpathyNFT is ERC721URIStorage, Ownable {
-    uint256 private _id;
+contract EmpathyNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    constructor() ERC721("EmpathyFinance", "EMPF") Ownable(msg.sender) {}
+    constructor() ERC721("EmpathyNFT", "EMO") {}
 
-    function safeMint(address to, string memory uri) public onlyOwner returns (uint256) {
-        _id += 1;
-        uint256 tokenId = _id;
+    // ✅ 누구나 호출 가능 (onlyOwner 제거)
+    function safeMint(address to, string memory uri) external returns (uint256) {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
         return tokenId;
