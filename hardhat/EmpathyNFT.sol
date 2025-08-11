c6f5d7ba561b58e4b68b36a1b4b47e10b9f4d226
gksmfly <149565853+gksmfly@users.noreply.github.com>
2025-08-11 17:55:00 +0900

    Update EmpathyNFT.sol

--- hardhat/EmpathyNFT.sol
+++ hardhat/EmpathyNFT.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EmpathyNFT is ERC721URIStorage, Ownable {
+    uint256 private _id;
+    constructor() ERC721("EmpathyFinance", "EMPF") Ownable(msg.sender) {}
+
+    function safeMint(address to, string memory uri) public onlyOwner returns (uint256) {
+        _id += 1;
+        uint256 tokenId = _id;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        return tokenId;
+    }
+}
