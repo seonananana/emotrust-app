14cf9c582992b39dd44cd1222d68a1df5d9407ab
gksmfly <149565853+gksmfly@users.noreply.github.com>
2025-08-12 00:28:56 +0900

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,11 +3,10 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
-  // (원하면 기본 paths 그대로 써도 OK)
   networks: {
     sepolia: {
       url: process.env.RPC_URL,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    }
-  }
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
