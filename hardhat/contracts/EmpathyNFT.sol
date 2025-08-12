c8ba76bbe87a0b2b5b7d3165116011eafe4314ad
gksmfly <149565853+gksmfly@users.noreply.github.com>
2025-08-12 13:57:04 +0900

    Update EmpathyNFT.sol

--- hardhat/contracts/EmpathyNFT.sol
+++ hardhat/contracts/EmpathyNFT.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract EmpathyNFT extends ERC721URIStorage {
+contract EmpathyNFT is ERC721URIStorage {
     uint256 private _nextTokenId;
 
     constructor() ERC721("EmpathyNFT", "EMO") {}
 
-    /// 팀 개발용: 테스트넷/로컬에서 누구나 민팅 가능 (필요시 onlyOwner로 바꾸면 됨)
+    // 테스트넷/로컬에서 누구나 민팅 가능 (필요하면 onlyOwner 붙이면 됨)
     function safeMint(address to, string memory tokenURI_) public returns (uint256) {
         uint256 tokenId = ++_nextTokenId;
         _safeMint(to, tokenId);
